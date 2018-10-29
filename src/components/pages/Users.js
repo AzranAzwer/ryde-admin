3f6f101678377a0061dedfdd96a102e5f9fe131e
sherange <sherangef@gmail.com>
2018-10-29 11:48:44 +0530

    changes

--- src/components/pages/Users.js
+++ src/components/pages/Users.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default () => {
-  return (
-    <div className="content-wrapper">
-      {/* <!-- Content Header (Page header) --> */}
-      <section className="content-header">
-        <h1>Users </h1>
-      </section>
-      <p>HI Azran</p>
-    </div>
-  );
-};
+export default class Users extends Component {
+  render() {
+    console.log('this.props', this.props);
+    return (
+      <div className="content-wrapper">
+        {/* <!-- Content Header (Page header) --> */}
+        <section className="content-header">
+          <h1>Users </h1>
+        </section>
+        <p>HI Azran</p>
+      </div>
+    );
+  }
+}
